74ae1cb22203f4f03fac97290d8470b94bc47b5e
Kenichi Abe <kenichi.abe@kenputer.dev>
2023-02-14 12:47:54 +0900

    support columnNames option

--- src/core.js
+++ src/core.js
@@ -81,6 +81,7 @@ luckysheet.create = function (setting) {
     Store.limitSheetNameLength =  extendsetting.limitSheetNameLength;
     Store.defaultSheetNameMaxLength = extendsetting.defaultSheetNameMaxLength;
     Store.fontList = extendsetting.fontList;
+    Store.columnNames = extendsetting.columnNames;
     server.gridKey = extendsetting.gridKey;
     server.loadUrl = extendsetting.loadUrl;
     server.updateUrl = extendsetting.updateUrl;
